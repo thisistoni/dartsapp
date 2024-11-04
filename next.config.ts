eb821935fd11af635469f029625e4f55dc8dc4c3
Antonio Beslic <thisistoni30@gmail.com>
2024-11-04 09:33:31 +0100

    add PWA support with next-pwa configuration and service worker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // Hier kannst du weitere Next.js-Konfigurationen hinzufügen
+});
 
 export default nextConfig;
