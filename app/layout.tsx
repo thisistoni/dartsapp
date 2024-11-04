86d4ab09e83e9f17ba2ea864cc916e480b154432
Antonio Beslic <thisistoni30@gmail.com>
2024-11-04 09:48:06 +0100

    add additional icons and update metadata for PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,26 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "WDV Landesliga Tool",
+  manifest: '/manifest.json',
+  themeColor: '#000000',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black',
+    title: 'Meine App',
+  },
   icons: {
     icon: '/icon.png', // der Pfad zum Favicon
+
+    apple: [
+      { url: '/icons/icon-192x192.png', sizes: '192x192' },  // Angepasst an manifest
+      { url: '/icons/icon-512x512.png', sizes: '512x512' },  // Angepasst an manifest
+      { url: '/icons/icon-180x180.png', sizes: '180x180' },  // iOS spezifisch
+    ],
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
   }
 };
 
